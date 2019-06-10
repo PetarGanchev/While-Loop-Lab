0e1500b4bf47da5bc35c5ff3a54009774d995198
Petar Ganchev <PetarGanchev@users.noreply.github.com>
2019-06-10 22:51:59 +0300

    progr

---
+++ 07. Graduation pt.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _06._Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int counter = 1;
+            double sumGrade = 0;
+            int countExcluded = 0;
+
+
+            while (counter <= 12)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                if (grade >= 4)
+                {
+                    sumGrade += grade;
+                    counter++;
+                }
+                else
+                {
+                    countExcluded++;
+                    if (countExcluded >= 2)
+                    {
+                        Console.WriteLine($"{name} has been excluded at {counter} grade");
+                        break;
+                    }
+                }
+
+
+            }
+            if (counter >= 12)
+            {
+                Console.WriteLine($"{name} graduated. Average grade: {sumGrade / 12:f2}");
+            }
+
+
+
+        }
+    }
+}
